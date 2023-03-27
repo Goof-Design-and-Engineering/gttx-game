cfe002f7af76f578a73070798d10a3568e95d61e
Zach Hoover <38825744+ZachHoover@users.noreply.github.com>
2023-03-27 18:09:57 -0500

    Flask app that can start/stop "rooms"
    Didn't want to override app.py yet. This flask app can start serving JSON from the scenarios for rooms.

--- pb_lib.py
+++ pb_lib.py
@@ -1,15 +1,18 @@
 from pocketbase import PocketBase
 
-#This will be either in this file or app.py
-class RoomInstance():
-    pass
-
 #A lot of these functions will be basically renaming the functions.
 def pullRoom(id):
     return client.collection("room").get_one(id)
 
 def updateRoom(room, item, value):
     client.collection("room").update(room.id, {item: value})
+
+def checkRoom(id):
+    try:
+        client.collection("room").get_one(id)
+        return True
+    except:
+        return False
 
 
 #Log into the database as an admin
@@ -18,6 +21,5 @@ with open("creds.txt", "r") as creds:
     #creds.gitignore is a file with two lines in it. line 0 is the username, line 1 is the password.
     admin_data = client.admins.auth_with_password(creds.readline().replace("\n", ""), creds.readline().replace("\n", ""))
 
-#example run
-#result = pullRoom("u7c9u8f90mp067l")
-#updateRoom(result, "uniqueid", "6969696969696969")
+id = "81lp924dbt8riiy"
+print(checkRoom(id))
